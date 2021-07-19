4987a20e3fd5e9cf41858ebdfe11d8483eb71d49
muraddurrani <77261055+muraddurrani@users.noreply.github.com>
2021-07-19 19:01:47 +0800

    Implement create appointment from Chats
    Added the ability to create an appointment with a user from the messaging screen.

--- src/screens/Client/ChatStack/MessageScreen/index.js
+++ src/screens/Client/ChatStack/MessageScreen/index.js
@@ -1,14 +1,20 @@
 import React, { useState, useLayoutEffect } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Icon } from 'react-native-elements'
 import firestore from '@react-native-firebase/firestore'
 import auth from '@react-native-firebase/auth'
 import ScreenView from '../../../../components/views/ScreenView'
 import Header from '../../../../components/molecules/Header'
 import MessageView from './MessageView'
 import MessageInput from './MessageInput'
+import TertiaryButton from '../../../../components/buttons/TertiaryButton'
+import colours from '../../../../styles/colours'
+import { mapObjectToSentence } from '../../../../utilities/helper'
 
 function index({ navigation, route }) {
   const {ID, msgRef, fullName, profilePhoto} = route.params
   const [messages, setMessages] = useState([])
+  const [showApp, setShowApp] = useState(false)
 
   const onSend = (message) => {
     const newMsg = {
@@ -40,13 +46,64 @@ function index({ navigation, route }) {
     return unsubscribe
   }, [])
 
+  const createAppointment = () => {
+    firestore().collection('Clients').doc(auth().currentUser.uid).collection('Matches').doc(ID).get().
+      then(doc => {
+        setShowApp(false)
+        navigation.navigate('Appointment', { screen: 'CA2', params: {ID: ID, photo: profilePhoto, name: fullName, service: mapObjectToSentence(doc.data().attributes[0])[0]}})
+      })
+  }
+
   return (
     <ScreenView>
-        <Header icon title = {fullName} profilePhoto = {profilePhoto} onPress = {() => navigation.goBack()} />
+        <Header icon title = {fullName} profilePhoto = {profilePhoto} onPress = {() => navigation.goBack()}
+           rightIcon = {
+            <View style = {styles.view}>
+              <Icon
+                name = 'calendar-plus-o'
+                type = 'font-awesome'
+                color = {colours.gray0}
+                containerStyle = {showApp ? styles.iconPressed : styles.icon}
+                onPress = {() => setShowApp(!showApp)}/>
+              {
+                showApp && <TertiaryButton title = "Create Appointment" style = {styles.appButton} titleStyle = {styles.title} onPress = {() => createAppointment()} />
+              }
+            </View>
+          }
+        />
         <MessageView messages = {messages} />
       <MessageInput onPress = {onSend} />
     </ScreenView>
   )
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flexDirection: 'row',
+    position: 'absolute',
+    right: 20
+  },
+  icon: {
+    padding: 4,
+  },
+  iconPressed: {
+    marginLeft: 20,
+    padding: 4,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)'
+  },
+  appButton: {
+    backgroundColor: colours.gray0,
+    padding: 10,
+    position: 'absolute',
+    right: 0,
+    top: 33,
+    borderWidth: 1,
+    borderColor: colours.gray2,
+    zIndex: 1
+  },
+  title: {
+    fontWeight: '400'
+  }
+})
 
 export default index
